d88064d2a05b7edeed8ac68236b1d4486a035c4f
dia36u <dia.babacar1@gmail.com>
2022-09-14 12:08:40 +0200

    database ajout table lyrics

--- database_creation.py
+++ database_creation.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import sqlite3
+
 conn = sqlite3.connect('database.db')
 c = conn.cursor()
 c.execute('CREATE TABLE IF NOT EXISTS artists (artist,genres,songs,popularity,link)')
@@ -7,3 +8,9 @@ conn.commit()
 
 df = pd.read_csv("dataset/artists-data.csv")
 df.to_sql("artists", conn, if_exists='replace', index=False)
+
+c.execute('CREATE TABLE IF NOT EXISTS lyrics (alink,sname,slink,lyric,language)')
+conn.commit()
+
+df = pd.read_csv("dataset/lyrics-data.csv")
+df.to_sql("lyrics", conn, if_exists='replace', index=False)
